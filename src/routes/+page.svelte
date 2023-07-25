c3c21870d04e61aca08f41d51100873262b0d998
Ahmed Onour <ahmedonour.49@gmail.com>
2023-07-25 09:30:30 +0300

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let data;
+  </script>
+<main>
+  <a href="/auth" rel="noopener noreferrer">Login</a>
+  <a href="/login" rel="noopener noreferrer">Login 2</a>
+  <ul>
+    {#each data.countries as country}
+    <li>{country.name}</li>
+    {/each}
+  </ul>
+</main>
