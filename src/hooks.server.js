015d5edf3c634e15af51e4ca5f0d15442274e864
Ahmed Onour <ahmedonour.49@gmail.com>
2023-07-25 09:47:00 +0300

    SSA

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,9 @@
 // src/hooks.server.js
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+import { redirect, error } from '@sveltejs/kit'
+
+// src/hooks.server.js
 
 export const handle = async ({ event, resolve }) => {
 	event.locals.supabase = createSupabaseServerClient({
@@ -8,22 +11,23 @@ export const handle = async ({ event, resolve }) => {
 		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
 		event
 	});
+  // protect requests to all routes that start with /protected-routes
+  if (event.url.pathname.startsWith('/protected-routes')) {
+    const session = await event.locals.getSession()
+    if (!session) {
+      // the user is not signed in
+      throw redirect(303, '/')
+    }
+  }
 
-	/**
-	 * a little helper that is written for convenience so that instead
-	 * of calling `const { data: { session } } = await supabase.auth.getSession()`
-	 * you just call this `await getSession()`
-	 */
-	event.locals.getSession = async () => {
-		const {
-			data: { session }
-		} = await event.locals.supabase.auth.getSession();
-		return session;
-	};
+  // protect POST requests to all routes that start with /protected-posts
+  if (event.url.pathname.startsWith('/protected-posts') && event.request.method === 'POST') {
+    const session = await event.locals.getSession()
+    if (!session) {
+      // the user is not signed in
+      throw error(303, '/')
+    }
+  }
 
-	return resolve(event, {
-		filterSerializedResponseHeaders(name) {
-			return name === 'content-range';
-		}
-	});
-};
+  return resolve(event)
+}
