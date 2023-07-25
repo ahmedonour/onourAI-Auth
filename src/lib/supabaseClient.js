f93b4c69acde5561dcb94a6f510de3944b691fe6
Ahmed Onour <ahmedonour.49@gmail.com>
2023-07-25 09:33:45 +0300

    formating

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,6 @@
-  import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-  export const supabase = createClient('https://qdckntajotiqzssqiult.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkY2tudGFqb3RpcXpzc3FpdWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAxMjk0NzAsImV4cCI6MjAwNTcwNTQ3MH0.SBU4TcE601jwi56nXqyiFUV1x4ozi4tRfSQZRcU-Vm0')
+export const supabase = createClient(
+	'https://qdckntajotiqzssqiult.supabase.co',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkY2tudGFqb3RpcXpzc3FpdWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAxMjk0NzAsImV4cCI6MjAwNTcwNTQ3MH0.SBU4TcE601jwi56nXqyiFUV1x4ozi4tRfSQZRcU-Vm0'
+);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
-    export let data;
-  </script>
+	export let data;
+</script>
+
 <main>
-  <a href="/auth" rel="noopener noreferrer">Login</a>
-  <a href="/login" rel="noopener noreferrer">Login 2</a>
-  <ul>
-    {#each data.countries as country}
-    <li>{country.name}</li>
-    {/each}
-  </ul>
+	<a href="/auth" rel="noopener noreferrer">Login</a>
+	<a href="/login" rel="noopener noreferrer">Login 2</a>
+	<ul>
+		{#each data.countries as country}
+			<li>{country.name}</li>
+		{/each}
+	</ul>
 </main>
